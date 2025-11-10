e20f20631b7ba0bf30fce3a4a0a63e764c23c057
sujit <sujitmanmode1997@gmail.com>
2025-11-10 09:49:08 +0530

    todays update 10/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_03_Annotation.java
@@ -0,0 +1,46 @@
+package TESTNG_Revision_November;
+
+import org.testng.annotations.*;
+
+public class TestNG_03_Annotation {
+    @BeforeSuite
+    public void Beforesuite(){
+        System.out.println("This is before suite");
+    }
+    @AfterSuite
+    public void Aftersuite(){
+        System.out.println("This is after suite");
+    }
+    @BeforeTest
+    public void beforetest(){
+        System.out.println("This is before test");
+    }
+    @AfterTest
+    public void aftertest(){
+        System.out.println("This is after test");
+    }
+    @BeforeMethod
+    public void beforemethod(){
+        System.out.println("This is before method");
+    }
+    @AfterMethod
+    public void aftermethod(){
+        System.out.println("THis is after method");
+    }
+    @BeforeGroups(groups = "smoke")
+    public void beforegroup(){
+        System.out.println("This is before group");
+    }
+    @AfterGroups(groups = "smoke")
+    public void aftergroup(){
+        System.out.println("This is after group");
+    }
+    @Test(groups = "smoke")
+    public void test1(){
+        System.out.println("THis is test1");
+    }
+    @Test
+    public void test2(){
+        System.out.println("This is test2");
+    }
+}
