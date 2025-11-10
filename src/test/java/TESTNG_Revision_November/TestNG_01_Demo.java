db886e3896fbb29e6fae9222802b29eb7ddb341c
sujit <sujitmanmode1997@gmail.com>
2025-11-10 08:34:05 +0530

    todays update 10/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_01_Demo.java
@@ -0,0 +1,10 @@
+package TESTNG_Revision_November;
+
+import org.testng.annotations.Test;
+
+public class TestNG_01_Demo {
+    @Test
+    public void loginTest(){
+        System.out.println("login in is successful");
+    }
+}
