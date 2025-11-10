b2ea793723bef7098cc7a2afbed67d2153d9073a
sujit <sujitmanmode1997@gmail.com>
2025-11-10 09:51:29 +0530

    todays update 10/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_02_Login.java
@@ -0,0 +1,15 @@
+package TESTNG_Revision_November;
+
+import org.apache.hc.core5.reactor.Command;
+import org.testng.annotations.Test;
+
+public class TestNG_02_Login {
+    @Test(priority = 1)
+    public void loginTest(){
+        System.out.println("Login test");
+    }
+    @Test(priority = 0)
+    public void logout(){
+        System.out.println("logout test");
+    }
+}
