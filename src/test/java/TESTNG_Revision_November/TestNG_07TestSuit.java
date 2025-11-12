7669b8c3a50fbe5ff1ec8156a9001e92e9c8967e
sujit <sujitmanmode1997@gmail.com>
2025-11-12 09:46:06 +0530

    todays update 11/11/25

--- src/test/java/TESTNG_Revision_November/TestNG_07TestSuit.java
+++ src/test/java/TESTNG_Revision_November/TestNG_07TestSuit.java
@@ -1,4 +1,25 @@
 package TESTNG_Revision_November;
 
 public class TestNG_07TestSuit {
+    // Write this as TestNG.XML File
+//<suite name ="functional suite">
+//        <test  name = "functional Tests">
+//        <classes>
+//            <class name = "TESTNG_Revision_November.TestNG_01_BrowserSelect"/>
+//            <class name = "TESTNG_Revision_November.TestNG_02_Login"/>
+//            <class name = "TESTNG_Revision_November.TestNG_03_Annotation"/>
+//            <class name = "TESTNG_Revision_November.TestNG_04_Assertion"/>
+//            <class name = "TESTNG_Revision_November.TestNG_05_SoftAssert"/>
+//        </classes>
+//        </test>
+// </suite>
 }
+
+
+// And we can also create MasterTestNG Suite XML file where we add all suite file and run
+//<suite name = "Master Suite">
+//    <suite-files>
+//        <suite-file path="./testng.xml"> </suite-file>
+//        <suite-file path="./regression.xml"> </suite-file>
+//    </suite-files>
+//</suite>
