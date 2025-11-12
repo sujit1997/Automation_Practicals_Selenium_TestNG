0d88fb058f639bf3bb14b0181b9e0aeeab81f18d
sujit <sujitmanmode1997@gmail.com>
2025-11-12 15:22:01 +0530

    todays update 11/11/25

--- src/test/java/TESTNG_Revision_November/TestNG_08_SkipTestCase.java
+++ src/test/java/TESTNG_Revision_November/TestNG_08_SkipTestCase.java
@@ -1,5 +1,6 @@
 package TESTNG_Revision_November;
 
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class TestNG_08_SkipTestCase {
@@ -7,5 +8,25 @@ public class TestNG_08_SkipTestCase {
     @Test(enabled = false)
     public void test1(){
         System.out.println("This test is not skip if output get");
+    }
+
+    @Test()
+    public void test2(){
+        System.out.println("skipping this test forcefully");
+        throw new SkipException("Skipping this test");
+    }
+
+    Boolean datasetup = true;
+    @Test
+    public void test3(){
+
+        System.out.println("Skipping this test based on condition");
+        if (datasetup = true){
+            System.out.println("execute the test");
+        }
+        else {
+            System.out.println("");
+        }
+
     }
 }
