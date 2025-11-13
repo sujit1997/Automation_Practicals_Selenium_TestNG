9c695288b94a8ddfa236b97da6e29e4bb347cef6
sujit <sujitmanmode1997@gmail.com>
2025-11-13 22:47:38 +0530

    todays update 13/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_18_ReportTest.java
@@ -0,0 +1,27 @@
+package TESTNG_Revision_November;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestNG_18_ReportTest {
+    @Test
+    public void reportTest1(){
+        System.out.println("This is test1");
+    }
+    @Test
+    public void reportTest2(){
+        //Reporter.log("This is test2" +result.getName());// for every test output we can add this line in Listeners class
+        //Reporter.log("Status of execution is - " +result.getStatus());
+        System.out.println("This is test2");
+    }
+
+    @Test
+    public void reportTest3(){
+        System.out.println("This is test3");
+    }
+
+    @Test
+    public void reportTest4(){
+       System.out.println("This is test4");
+       }
+}
