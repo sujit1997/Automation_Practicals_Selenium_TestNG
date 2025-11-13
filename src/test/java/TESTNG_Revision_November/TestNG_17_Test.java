9541f0e3b8d4e6f1229d45977869af3e3d64288e
sujit <sujitmanmode1997@gmail.com>
2025-11-13 22:23:55 +0530

    todays update 13/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_17_Test.java
@@ -0,0 +1,14 @@
+package TESTNG_Revision_November;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestNG_17_Test {
+    @Test(retryAnalyzer = TestNG_17_executeFailTestCase.class)
+    public void test(){
+        System.out.println("run test");
+        Assert.assertTrue(false);
+    }
+
+}
