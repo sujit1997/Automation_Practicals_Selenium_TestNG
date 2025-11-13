74b152f7cd90d5b314d64b771b421e17bfeeb54e
sujit <sujitmanmode1997@gmail.com>
2025-11-13 17:48:57 +0530

    todays update 13/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_16_BaseTest.java
@@ -0,0 +1,21 @@
+package TESTNG_Revision_November;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestNG_16_BaseTest {
+    public static WebDriver driver = null;
+
+    @BeforeSuite
+    public void launchBrowser(){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+    @AfterSuite
+    public void closingBrowser(){
+        driver.close();
+    }
+}
