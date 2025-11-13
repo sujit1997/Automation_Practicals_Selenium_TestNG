0782fd2a70305ed1447c1320d03d8cff63f433e2
sujit <sujitmanmode1997@gmail.com>
2025-11-13 08:28:15 +0530

    todays update 12/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_11_DataProvider.java
@@ -0,0 +1,47 @@
+package TESTNG_Revision_November;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestNG_11_DataProvider {
+
+    @Test(dataProvider = "dataset")
+    public void test(String username, String password){
+        System.out.println(username+"==="+password);
+    }
+
+    @DataProvider
+    public Object[][] dataset(){
+        Object[][] dataset = new Object[4][2];
+
+        dataset[0][0]="user1";
+        dataset[0][1]="pass1";
+
+        dataset[1][0]="user2";
+        dataset[1][1]="pass2";
+
+        dataset[2][0]="user3";
+        dataset[2][1]="user3";
+
+        dataset[3][0]="user4";
+        dataset[3][1]="pass4";
+
+        return dataset;
+    }
+
+    @Test(dataProvider = "dataset1")
+    public void test1(String username, String password, String test){
+        System.out.println(username+"==="+password+"==="+test);
+    }
+    //Second way to provide data
+    @DataProvider
+    public Object[][] dataset1(){
+        return new Object[][]
+                {
+                        {"username","password","test"},
+                        {"username1","password1","test1"}
+                };
+    }
+
+
+}
