7fcc92ea2f470e60f7c7ecdc41aad6929b5a4a91
sujit <sujitmanmode1997@gmail.com>
2025-11-13 09:07:01 +0530

    todays update 12/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_12_DataProviderLiveDemo.java
@@ -0,0 +1,36 @@
+package TESTNG_Revision_November;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestNG_12_DataProviderLiveDemo {
+
+    @Test(dataProvider = "create")
+    public void test(String username, String password)
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+        driver.close();
+    }
+
+    @DataProvider(name="create")
+    public Object[][] dataset(){
+        return new Object[][]
+                {
+                        {"standard_user","secret_sauce"},
+                        {"locked_out_user","secret_sauce"},
+                        {"problem_user", "secret_sauce"},
+                        {"performance_glitch_user","secret_sauce"},
+                        {"error_user","secret_sauce"},
+                        {"visual_user","secret_sauce"}
+                };
+                }
+    }
