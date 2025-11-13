159da833bdbbd0079a89ef9a19731ee7c2ae5300
sujit <sujitmanmode1997@gmail.com>
2025-11-13 10:29:48 +0530

    todays update 13/11/25

---
+++ src/test/java/TESTNG_Revision_November/TestNG_13_DataProviderTest.java
@@ -0,0 +1,21 @@
+package TESTNG_Revision_November;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNG_13_DataProviderTest {
+    @Test(dataProvider = "create",dataProviderClass=TestNG_13_MultipleDataprovider.class)
+    public void test(String username, String password)
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("login-button")).click();
+        driver.close();
+    }
+}
---
+++ src/test/java/TESTNG_Revision_November/TestNG_13_MultipleDataprovider.java
@@ -0,0 +1,27 @@
+package TESTNG_Revision_November;
+
+import org.testng.annotations.DataProvider;
+
+import java.lang.reflect.Method;
+
+public class TestNG_13_MultipleDataprovider {
+
+    @DataProvider(name = "create")
+    public Object[][] dataset1(Method M) {
+        Object[][] testdata = null;
+
+        if (M.getName().equals("test")) {
+            testdata = new Object[][] {
+                    {"standard_user", "secret_sauce"},
+                    {"locked_out_user", "secret_sauce"}
+        };
+    }
+        else if (M.getName().equals("test1")){
+         testdata = new Object[][]{
+                        {"username", "password", "Third"},
+                        {"username", "password", "Fourth"}
+                    };
+    }
+            return testdata;
+        }
+    }
