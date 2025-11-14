eb2199e12db163a65b1aa1874944ec1147d34a65
sujit <sujitmanmode1997@gmail.com>
2025-11-14 10:41:40 +0530

    todays update 14/11/25

--- src/test/java/TESTNG_Revision_November/TestNG_20_Dependency.java
+++ src/test/java/TESTNG_Revision_November/TestNG_20_Dependency.java
@@ -1,27 +1,27 @@
-package TESTNG_Revision_November;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class TestNG_20_Dependency {
-    @Test
-    public void userRegistration(){
-        System.out.println("This is test1");
-        Assert.assertTrue(false);
-    }
-
-    @Test(dependsOnMethods = "userRegistration") // Hard dependency
-    public void userSearch(){
-        System.out.println("This is test2");
-    }
-
-    @Test
-    public void url(){
-        System.out.println("This is test3");
-        Assert.assertTrue(false);
-    }
-    @Test(dependsOnMethods = "url",alwaysRun = true) //soft dependency this test run even dependet mmethod is fail
-    public void urldependent(){
-        System.out.println("this is test 4");
-    }
-}
+//package TESTNG_Revision_November;
+//
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class TestNG_20_Dependency {
+//    @Test
+//    public void userRegistration(){
+//        System.out.println("This is test1");
+//        Assert.assertTrue(false);
+//    }
+//
+//    @Test(dependsOnMethods = "userRegistration") // Hard dependency
+//    public void userSearch(){
+//        System.out.println("This is test2");
+//    }
+//
+//    @Test
+//    public void url(){
+//        System.out.println("This is test3");
+//        Assert.assertTrue(false);
+//    }
+//    @Test(dependsOnMethods = "url",alwaysRun = true) //soft dependency this test run even dependet mmethod is fail
+//    public void urldependent(){
+//        System.out.println("this is test 4");
+//    }
+//}
