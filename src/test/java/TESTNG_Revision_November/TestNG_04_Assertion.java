d5a40cfcf64bc6b0ce29aacfdd4f35754b9d65db
sujit <sujitmanmode1997@gmail.com>
2025-11-15 14:42:27 +0530

    todays update 15/11/25

--- src/test/java/TESTNG_Revision_November/TestNG_04_Assertion.java
+++ src/test/java/TESTNG_Revision_November/TestNG_04_Assertion.java
@@ -1,26 +1,26 @@
-//package TESTNG_Revision_November;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//@Test
-//public class TestNG_04_Assertion {
-//    public static String browser = "chrome";
-//    public static WebDriver driver;
-//    public void titletest(){
-//        browser.equalsIgnoreCase("chrome");
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//
-//        driver.get("https://www.ebay.com/");
-//        String actualTitle = driver.getTitle();
-//        String expectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
-//        Assert.assertEquals(actualTitle, expectedTitle);
-//        driver.close();
-//
-//
-//    }
-//}
+package TESTNG_Revision_November;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class TestNG_04_Assertion {
+    public static String browser = "chrome";
+    public static WebDriver driver;
+    public void titletest(){
+        browser.equalsIgnoreCase("chrome");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://www.ebay.com/");
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        driver.close();
+
+
+    }
+}
