98fd1365d95364fff88f3949b2174ab213ce903e
sujit <sujitmanmode1997@gmail.com>
2025-11-15 13:38:40 +0530

    todays update 15/11/25

---
+++ src/test/java/TESTNG_Revision/TestNG_03_Annotation.java
@@ -0,0 +1,39 @@
+package TESTNG_Revision;
+
+import org.testng.annotations.*;
+
+public class TestNG_03_Annotation {
+
+    @BeforeTest
+    public void connectDB(){
+        System.out.println("connect to database");
+    }
+
+    @BeforeMethod
+    public void loginApplication(){
+        System.out.println("login to application");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 performing");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 performing");
+    }
+
+    @AfterMethod
+    public void logoutApplication(){
+        System.out.println("logout from application");
+    }
+
+    @AfterTest
+    public void disconnectDB(){
+        System.out.println("Database is disconnect");
+    }
+
+
+
+}
